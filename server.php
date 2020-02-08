890e11557405ff9c13074cf4e650c8522f09285f
Paras Tiwari <parastiware@gmail.com>
2020-02-08 12:45:07 +0545

    commit\

--- server.php
+++ server.php
@@ -1,8 +1,15 @@
 <?php
 $username="";
 $email="";
-$error = array();
+$error = array();//error array
+//database connected
 $db=mysqli_connect("localhost","root","","cv") or die("could not connect to database");
+//user registration logics
+$username=mysqli_real_escape_string($db,$_POST['username']);
+$email=mysqli_real_escape_string($db,$_POST['email']);
+$password_1=mysqli_real_escape_string($db,$_POST['pass1']);
+$password_2=mysqli_real_escape_string($db,$_POST['pass2']);
+
 
 
 
