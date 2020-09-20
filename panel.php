23dbf69bee71a779ae1ee7adcc269b1170f1bdcf
Paras Tiwari <parastiware@gmail.com>
2020-09-20 18:30:51 +0545

    Addes panel.css

--- panel.php
+++ panel.php
@@ -25,6 +25,7 @@ if(isset($_GET['logout'])){
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="includes/css/panel.css">
    <script type="text/javascript">
     function dataEntry() {
     $username=$document.getElementById("")
@@ -37,7 +38,7 @@ if(isset($_GET['logout'])){
     
 </head>
 <body>
-    <div>
+    <div class="greeting">
         
         <?php if(isset($_SESSION['username'])):?>   
         <h1>Welcome to your admin portal <?php echo($_SESSION['username']); ?>
@@ -45,29 +46,37 @@ if(isset($_GET['logout'])){
        $query="SELECT * FROM admintab";
        $result=mysqli_query($db,$query);
        ?>
+
+    <div class="user-table">
        <h2>List of registered users</h2>
-         <table>
+         <table class="table">
          <thead>
          <tr>
-         <th>Username</th>
-        <th>Email</th>
-        <th>Action</th>
-         </tr>
-         </thead>
+         <th scope="col">#</th>
+         <th scope="col">Username</th>
+        <th scope="col">Email</th>
+        <th scope="col">Action</th>
+        </tr>
+        </thead>
          
         <?php
         while($data=mysqli_fetch_assoc($result))
         {
         ?>
+        <tbody>
+        
         <tr>
-        <td><?php echo($data['username']); ?></td>
+        <th scope="row">1</th>
+        <td ><?php echo($data['username']); ?></td>
         <td><?php echo($data['email']); ?></td>
         <td>
-            <a href="panel.php?Edit=true&id=<?php echo $data['user_id']?>"><button name="makeadmin" >Makeadmin</button></a>
-            <a href="panel.php?Delete=true&id=<?php echo $data['user_id']?>"><button name="Delete">delete</button></a>
-            
+            <a href="panel.php?Edit=true&id=<?php echo $data['user_id']?>"><button name="makeadmin"type="button" class="btn btn-primary" >Makeadmin</button></a>
+            <a href="panel.php?Delete=true&id=<?php echo $data['user_id']?>"><button name="Delete" type="button" class="btn btn-danger">delete</button></a>
         </td>
         </tr>
+        </tbody>
+        </table>
+    </div>
         <?php
         }
         
@@ -111,14 +120,14 @@ if(isset($_GET['logout'])){
                 header("Refresh: $sec; url=$page");
         }
         ?>
-        
-        </table>
-        <button><a href="panel.php?logout='1'">LOG OUT</a></button>
-        <button><a href="registration.php">Registration</a></button>
-        <button><a href="home.php">View site</a></button>
+
+       
+        <button type="button" class="btn btn-primary"><a href="panel.php?logout='1'">LOG OUT</a></button>
+        <button  type="button" class="btn btn-primary"><a href="registration.php">Registration</a></button>
+        <button  type="button" class="btn btn-primary"><a href="home.php">View site</a></button>
         <?php endif ?>
         
-    </div>
+
     
     
 </body>
