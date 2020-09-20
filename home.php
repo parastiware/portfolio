0c068eb1b31fb5ea7d543dd39b34de9c6752d632
Paras Tiwari <parastiware@gmail.com>
2020-09-20 13:51:08 +0545

    added carousel

--- home.php
+++ home.php
@@ -41,38 +41,31 @@ $_SESSION['date']="";
                         ?>
                         <p>I am <?php echo($data['Fname']); echo("&nbsp;");  echo($data['Mname']); echo("&nbsp;");  echo($data['Lname']);?> and welcome to my site.</p>
         </div>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <div class="item active">
-                    <img src="includes/images/carousel/img1.jpg" alt="img1">
+        <div id="carouselExampleIndicator" class="carousel slide" data-ride="carousel " >
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicator" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicator" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicator" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="includes/images/carousel/img1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="includes/images/carousel/img2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="includes/images/carousel/img3.jpg" class="d-block w-100" alt="...">
+                        </div>
                     </div>
-
-                    <div class="item">
-                    <img src="includes/images/carousel/img2.jpg" alt="img2">
-                    </div>
-
-                    <div class="item">
-                    <img src="includes/images/carousel/img3.jpg" alt="img3">
-                    </div>
-                </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicator" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicator" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="fasle"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
         </div>
 
         <div class="About">
